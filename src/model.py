8948e299517c41d84e1ce4d2431de4e90e6c66b0
Rudrakshi Rai <bennettrigel23@gmail.com>
2025-09-14 17:47:31 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -10,7 +10,7 @@ class BoneCNN(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
 
-        # Add a dummy forward pass to compute fc1 input size
+        #dummy forward pass to compute fc1 input size
         self._to_linear = None
         self._dummy_forward()
 
@@ -18,8 +18,8 @@ class BoneCNN(nn.Module):
         self.fc2 = nn.Linear(128, num_classes)
 
     def _dummy_forward(self):
-        # create a dummy tensor with the same size as your input images
-        x = torch.zeros(1, 3, 128, 128)  # if your images are 128x128
+        #dummy tensor with the same size as input images
+        x = torch.zeros(1, 3, 128, 128)  # 
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
